e702ddbc4571826efa849a9ecced3a463255c85a
Dylan Nguyen <dylanminhnguyen@gmail.com>
2016-08-07 12:59:27 -0700

    Created EmployeeGUI

---
+++ Project/src/EmployeeGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by dmnguyen on 8/7/16.
+ */
+public class EmployeeGUI {
+    private JPanel EmployeePanel;
+    private JPanel northPanel;
+    private JPanel southPanel;
+    private JButton scheduleButton;
+    private JButton meetingButton;
+    private JButton userAccountButton;
+}
