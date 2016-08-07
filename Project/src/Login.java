5de99f0d17fdae479b097c13c3b2aef9d4ba23c9
Dylan Nguyen <dylanminhnguyen@gmail.com>
2016-08-07 11:09:00 -0700

    Creating Login UI Form
    Login UI Form

--- Project/src/Login.java
+++ Project/src/Login.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 /**
  * Created by dmnguyen on 7/7/16.
  */
 public class Login {
+    private JPanel loginPanel;
+    private JPanel labelPanel;
+    private JPanel fieldPanel;
 }
