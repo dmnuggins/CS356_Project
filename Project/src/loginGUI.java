178673ea5db651ee2e2f703b34c940fde85e2fc8
Dylan Nguyen <dylanminhnguyen@gmail.com>
2016-08-07 11:35:22 -0700

    Rename Login to loginGUI
    Refactor

---
+++ Project/src/loginGUI.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+/**
+ * Created by dmnguyen on 7/7/16.
+ */
+public class loginGUI {
+    private JPanel loginPanel;
+    private JPanel westLabelPanel;
+    private JPanel southButtonPanel;
+    private JPanel eastFieldPanel;
+    private JTextField userNameField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+    private JButton cancelButton;
+    private JLabel userNameLabel;
+    private JLabel passwordLabel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
