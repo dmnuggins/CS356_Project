9fa1cdad7abae9f85e52bbba3286f23c34cfecd2
Christian Hill <Christian.T.Hill@jpl.nasa.gov>
2016-08-09 16:44:05 -0700

    Fixed more filenames

---
+++ Project/src/AdminGUI.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by CesarRecinos on 8/7/2016.
+ */
+public class AdminGUI extends JFrame{
+    private JPanel rootPanel;
+    private JPanel employeePanel;
+    private JPanel buttonPanel;
+    private JButton employeeButton;
+    private JPanel meetingPanel;
+    private JPanel backPanel;
+    private JButton meetingButton;
+    private JButton goBack;
+
+    public AdminGUI() {
+
+        super("Admin Menu");
+        setContentPane(rootPanel);
+        pack();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        employeeButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                EmployeeDisplayAdminGUI empDisplay = new EmployeeDisplayAdminGUI();
+            }
+        });
+
+
+        meetingButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+
+                RoomDisplayAdminGUI roomDisplay =new RoomDisplayAdminGUI();
+
+            }
+        });
+
+        goBack.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        setVisible(true);
+    }
+}
