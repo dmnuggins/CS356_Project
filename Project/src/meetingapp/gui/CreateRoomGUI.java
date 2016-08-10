26757f630b6facad1128c436d4d36ecd34849494
carecinos <cesar.recinos@yahoo.com>
2016-08-09 20:23:52 -0700

    Created CreateRoomGUI

---
+++ Project/src/meetingapp/gui/CreateRoomGUI.java
@@ -0,0 +1,16 @@
+package meetingapp.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by CesarRecinos on 8/9/2016.
+ */
+public class CreateRoomGUI {
+    private JPanel rootPanel;
+    private JTextField textField1;
+    private JTextField capacityField;
+    private JLabel roomIDLabel;
+    private JLabel capacityLabel;
+    private JButton saveButton;
+    private JButton cancelButton;
+}
